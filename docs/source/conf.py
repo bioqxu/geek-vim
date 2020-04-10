f661d534f498c121915be9523024e2a97913ef08
Quan Xu <qxuchn@gmail.com>
2020-04-10 09:53:13 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,16 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+
+
+
